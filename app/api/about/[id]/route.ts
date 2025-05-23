3ec9b4db82d9c42ead5b3b3ea2fb8a44d71abebb
Paschal <paschalnwokeocha1@gmail.com>
2025-05-23 11:43:01 +0100

    about

--- app/api/about/[id]/route.ts
+++ app/api/about/[id]/route.ts
@@ -1,16 +1,19 @@
-import  dbConnect from '@/lib/db';
+import dbConnect from '@/lib/db';
 import { About } from '@/models/About';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
-  await dbConnect;
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  await dbConnect(); // <== This was missing `()`
+  const { id } = context.params;
   const body = await req.json();
-  const updated = await About.findByIdAndUpdate(params.id, body, { new: true });
+  const updated = await About.findByIdAndUpdate(id, body, { new: true });
   return NextResponse.json(updated);
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
-  await dbConnect;
-  await About.findByIdAndDelete(params.id);
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  await dbConnect();
+  const { id } = context.params;
+  await About.findByIdAndDelete(id);
   return NextResponse.json({ message: 'Deleted successfully' });
 }
