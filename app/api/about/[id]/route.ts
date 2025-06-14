48807865a7baf6e02a038f71d29c62fa2c25fee9
Paschal <paschalnwokeocha1@gmail.com>
2025-06-14 14:29:05 +0100

    lets go

--- app/api/about/[id]/route.ts
+++ app/api/about/[id]/route.ts
@@ -1,40 +1,26 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/db";
 import { About } from "@/models/About";
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     const data = await req.json();
     await dbConnect();
-    const updatedAbout = await About.findByIdAndUpdate(params.id, data, {
-      new: true,
-    });
+    const updatedAbout = await About.findByIdAndUpdate(params.id, data, { new: true });
     return NextResponse.json(updatedAbout);
   } catch (error) {
     console.error(error);
-    return NextResponse.json(
-      { message: "Failed to update about" },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: "Failed to update about" }, { status: 500 });
   }
 }
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     await dbConnect();
     await About.findByIdAndDelete(params.id);
     return NextResponse.json({ message: "Deleted successfully" });
   } catch (error) {
     console.error(error);
-    return NextResponse.json(
-      { message: "Failed to delete about" },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: "Failed to delete about" }, { status: 500 });
   }
 }
